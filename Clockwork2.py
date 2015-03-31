78de8eeddd116ea91794706e18ddfaeade929e84
Avraam Tsantekidis <makistsantekidis@gmail.com>
2015-03-31 21:46:03 +0300

    regularization term to preserve norm of gradient through time smart tanh initialization EMA for setting norm threshold of both gradients and their 'adam' momentum

--- Clockwork2.py
+++ Clockwork2.py
@@ -10,24 +10,14 @@ from theano import tensor as T
 import matplotlib.pyplot as plt
 from theano.tensor.nnet import softmax
 
-from utils import adam, quadratic_loss, variance
+from utils import adam, quadratic_loss, variance, float32, int32, relu, sgd
 
+
+rng = np.random.RandomState(int(time()))
 
 sys.setrecursionlimit(100000)
 
 floatX = theano.config.floatX
-
-
-def float32(k):
-    return np.cast['float32'](k)
-
-
-def int32(k):
-    return np.cast['int32'](k)
-
-
-def relu(x):
-    return T.maximum(0, x)
 
 
 class ClockworkLayer(object):
@@ -43,18 +33,36 @@ class ClockworkLayer(object):
         # name="{}_W_in".format(groups_num))
 
         # Weights for recurrent connection within the group
-        self.W_self = theano.shared(np.random.normal(loc=0.0, scale=variance(groups_num * group_size),
-                                                     size=(groups_num * group_size, groups_num, group_size)).astype(
-            floatX))
 
-        # name="{}_W_self".format(groups_num))
+        self.W_self = np.random.normal(loc=0.0, scale=0.01,
+                                       size=(groups_num * group_size, groups_num, group_size)).astype(floatX)
+        self.W_self_nullifier = np.zeros(self.W_self.shape, dtype=floatX)
+        for dx in xrange(groups_num * group_size):
+            for g in xrange(groups_num):
+                if g >= (dx // group_size):
+                    self.W_self[dx][g] = 0.
+                else:
+                    self.W_self_nullifier[dx, g] = 1.
+                    spng = rng.permutation(group_size)
+                    self.W_self[dx][g][spng[15:]] = 0.
+
+        self.W_self = theano.shared(self.W_self,
+                                    name="{}_W_self".format(groups_num))
+        # self.W_self = theano.shared(np.random.normal(loc=0.0, scale=0.01,
+        # size=(groups_num * group_size, groups_num, group_size)).astype(
+        # floatX),
+        #     name="{}_W_self".format(groups_num))
+        #
+
+
         self.biases = theano.shared(
             np.zeros((groups_num, group_size), dtype=floatX))
 
         self.initial_activation = theano.shared(np.random.normal(loc=0.0, scale=variance(groups_num * group_size),
-                                                                 size=groups_num * group_size).astype(floatX))
+                                                                 size=groups_num * group_size).astype(floatX),
+                                                name='init_activation')
 
-        self.params = [self.W_in, self.W_self, self.biases, self.initial_activation]
+        self.params = [self.W_self, self.W_in, self.biases, self.initial_activation]
         self.timestep = theano.shared(1)
 
     def fprop(self, input_steps):
@@ -83,11 +91,18 @@ class ClockworkLayer(object):
             time_step += 1
             return new_activation, time_step
 
-        initial_activation = T.ones((theano_input.shape[1], self.initial_activation.shape[0])) * self.initial_activation
+        initial_activation = T.ones((theano_input.shape[1], self.initial_activation.shape[0]),
+                                    dtype=floatX) * self.initial_activation
         output_steps, updates = theano.scan(step, sequences=[theano_input],
                                             outputs_info=[initial_activation, self.timestep],
                                             non_sequences=[self.W_in, self.W_self, self.biases])
         output_steps = output_steps[0]
+        scan_node = output_steps.owner.inputs[0].owner
+        assert isinstance(scan_node.op, theano.scan_module.scan_op.Scan)
+        n_pos = scan_node.op.n_seqs + 1
+        init_h = scan_node.inputs[n_pos]
+        self.init_h = init_h
+        self.activation = output_steps
         return output_steps, updates
 
 
@@ -119,7 +134,9 @@ class OutputLayer(object):
 
 
 class ClockworkRNN(object):
-    def __init__(self, layer_specs=(1, (2, 20), (2, 10), 1), cost=quadratic_loss, update_fn=adam, learning_rate=0.001):
+    def __init__(self, layer_specs=(1, (2, 20), (2, 10), 1), cost=quadratic_loss, update_fn=adam, learning_rate=0.001,
+                 alpha=1.):
+        self.alpha = theano.shared(float32(alpha))
         self.layer_specs = layer_specs
         self.layers = []
         previous_size = layer_specs[0]
@@ -127,10 +144,11 @@ class ClockworkRNN(object):
         self.cost = cost
         self.update_fn = update_fn
         self.learning_rate = theano.shared(float32(learning_rate))
+        self.training_step = theano.shared(float32(1))
 
         for i, spec in enumerate(layer_specs[1:-1]):
             spec = [spec[0], spec[1], previous_size]
-            self.layers.append(ClockworkLayer(*spec))
+            self.layers.append(ClockworkLayer(*spec, activation_function=T.tanh))
             self.params.extend(self.layers[-1].params)
             previous_size = spec[0] * spec[1]
         self.layers.append(OutputLayer(layer_specs[-1], previous_size, T.tanh))
@@ -148,22 +166,57 @@ class ClockworkRNN(object):
         theano_input = theano_input.dimshuffle(1, 0, 2)
         previous_input = theano_input
         updates = {}
-        for layer in self.layers:
-            theano_input.tag.test_value = np.ones((2, 10, 1), dtype=floatX)
+        preq_reg_params = []
+        # theano_input.tag.test_value = np.ones((2, 10, 1), dtype=floatX)
+        for layer in self.layers[:-1]:
             previous_input, upd = layer._fprop(previous_input)
             updates.update(dict(upd))
+            preq_reg_params.append([previous_input, layer.init_h])
+        previous_input, upd = self.layers[-1]._fprop(previous_input)
+        updates.update(dict(upd))
         output = previous_input.dimshuffle(1, 0, 2)
-
+        self.preq_reg_params = preq_reg_params
         return output, updates
 
     def bptt(self, X, y):
         if not getattr(self, '_bptt', None):
-            theano_input = T.tensor3()
-            labeled_data = T.tensor3()
+            test_vals = create_batch_func_params(10, 0.1, 2)
+            theano_input = T.tensor3('x')
+            labeled_data = T.tensor3('y')
+            theano_input.tag.test_value = test_vals[0]
+            labeled_data.tag.test_value = test_vals[1]
             prediction, updates = self._fprop(theano_input)
             loss = self.cost(prediction, labeled_data)
-            updates = self.update_fn(loss, self.params, learning_rate=self.learning_rate)
-            self._bptt = theano.function([theano_input, labeled_data], [loss], updates=updates)
+            grads = T.grad(loss, self.params + sum(self.preq_reg_params, []))
+            reg_grads = grads[len(self.params):]
+            grads = grads[:len(self.params)]
+            # for i, (layer, preq) in enumerate(zip(self.layers[:-1], self.preq_reg_params)):
+            #     reg_grad = reg_grads[2 * i:2 * i + 2]
+            #     if layer.activation_function == T.tanh:
+            #         tmp_x = reg_grad[1][1:] * (1 - preq[0] ** 2)
+            #     else:
+            #         tmp_x = reg_grad[1][1:]
+            #
+            #     sh0 = tmp_x.shape[0]
+            #     sh1 = tmp_x.shape[1]
+            #     sh2 = tmp_x.shape[2]
+            #     tmp_x = tmp_x.reshape((sh0 * sh1, sh2))
+            #     tmp_x = T.dot(tmp_x, (layer.W_self * layer.W_self_nullifier).reshape((sh2, -1)))
+            #     tmp_x = (tmp_x.reshape((sh0, sh1, sh2)) ** 2).sum(2)
+            #     tmp_y = (reg_grad[1][1:] ** 2).sum(2)
+            #     tmp_reg = (T.switch(T.ge(tmp_y, 1e-20), tmp_x / tmp_y, 1) - 1.) ** 2
+            #     n_elems = T.mean(T.ge(tmp_y, 1e-20), axis=1)
+            #     tmp_reg = tmp_reg.mean(1).sum() / n_elems.sum()
+            #     tmp_gWhh = T.grad(tmp_reg, layer.W_self)
+            #     grads[4 * i] += self.alpha * T.exp(self.training_step/-1000.0) * tmp_gWhh
+
+            updates, norm, grad_threshold, momentum_norm, momentum_norm_threshold = self.update_fn(loss, self.params,
+                                                                                                   grads=grads,
+                                                                                                   learning_rate=self.learning_rate, training_step=self.training_step)
+            updates.append((self.training_step, self.training_step + 1))
+            self._bptt = theano.function([theano_input, labeled_data],
+                                         [loss, norm, grad_threshold, momentum_norm, momentum_norm_threshold],
+                                         updates=updates, allow_input_downcast=True)
             self._bptt.trust_input = True
         return self._bptt(X, y)
 
@@ -173,7 +226,7 @@ def func_to_learn(X):
 
 
 def create_batch_func_params(input_length=300, freq_var=0.1, size=20):
-    freqs = float32(np.abs(np.random.normal(scale=freq_var, size=size)) + 0.2)
+    freqs = float32(np.abs(np.random.normal(scale=freq_var, size=size)) + 0.1)
     # freqs = np.ones(size, dtype=floatX) * float32(0.1)
     X = np.array([np.ones(input_length, dtype=floatX) * freq for freq in freqs], dtype=floatX)[:, :, np.newaxis]
     x_series = np.array([np.linspace(0, input_length * freq, num=input_length, dtype=floatX) for freq in freqs],
@@ -183,25 +236,34 @@ def create_batch_func_params(input_length=300, freq_var=0.1, size=20):
 
 
 def main():
-    net = ClockworkRNN((1, (3, 40), 1))
+    net = ClockworkRNN((1, (4, 30), 1), update_fn=adam, learning_rate=0.001)
     # ones = np.ones((2, 10, 1), dtype=floatX)
     # res = net.fprop(ones)
     losses = []
-    X, y, x_series = create_batch_func_params(400, 0.1, 400)
+    lrs = []
+    norms, momentum_norms = [], []
+    norms_thres, momentum_norms_thres = [], []
+    # X, y, x_series = create_batch_func_params(500, 0.1, 2)
+    X, y, x_series = create_batch_func_params(410, 0.1, 3)
     best = np.inf
     last_best_index = 0
-    decrement = float32(0.99)
-    for i in range(21000):
+    decrement = float32(0.95)
+    for i in range(2000):
         start = time()
-
-        losses.append(net.bptt(X, y))
+        loss, norm, norm_theshold, momentum_norm, momentum_norm_threshold = net.bptt(X, y)
+        losses.append(loss)
+        norms.append(norm)
+        momentum_norms.append(momentum_norm)
+        norms_thres.append(float32(norm_theshold))
+        momentum_norms_thres.append(float32(momentum_norm_threshold))
+        lrs.append(net.learning_rate.get_value())
         epoch_time = time() - start
         print i, ':', losses[-1], " took :", epoch_time
 
         if best > losses[-1]:
             last_best_index = i
             best = losses[-1]
-        elif i - last_best_index > 9:
+        elif i - last_best_index > 30:
             best = losses[-1]
             new_rate = net.learning_rate.get_value() * decrement
             net.learning_rate.set_value(new_rate)
@@ -211,23 +273,72 @@ def main():
     with open('rnn.pickle', 'wb') as f:
         pickle.dump(net, f)
 
-    plt.figure(figsize=(100, 20), dpi=100)
-    plt.plot(losses)
-    plt.savefig('rnn_quadratic_cost.jpg')
+    # plt.figure(figsize=(40, 20), dpi=100)
+    fig, ax1 = plt.subplots(figsize=(30, 10))
+    ax2 = ax1.twinx()
+
+    ax1.plot(losses, label='loss')
+    ax1.set_ylabel('Loss')
+
+    ax2.plot(lrs, color="red", label='learning rate')
+    ax2.set_ylabel('Learning rate')
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1 + h2, l1 + l2, loc='best')
+
+    fig.savefig('rnn_learning_rate_and_cost.jpg')
     plt.clf()
-    X, y, x_series = create_batch_func_params(1200, 0.1)
+
+    fig, ax1 = plt.subplots(figsize=(30, 10))
+    ax2 = ax1.twinx()
+
+    ax1.plot(losses[-1000:], label='loss')
+    ax1.set_ylabel('Loss')
+
+    ax2.plot(lrs[-1000:], color="red", label='learning rate')
+    ax2.set_ylabel('Learning rate')
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1 + h2, l1 + l2, loc='best')
+
+    fig.savefig('rnn_learning_rate_and_cost_last1000.jpg')
+    plt.clf()
+
+    # NORMS
+    fig, ax1 = plt.subplots(figsize=(30, 10))
+    ax2 = ax1.twinx()
+
+    ax1.plot(norms, label="norm", color='red')
+    ax1.plot(norms_thres, label="norm threshold", color='blue')
+    ax1.set_ylabel('Gradient norm')
+
+    ax2.plot(momentum_norms, label="momentum norms", color='green')
+    ax2.plot(momentum_norms_thres, label="momentum norms threshold", color='purple')
+    ax2.set_ylabel('Momentum norm')
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    ax1.legend(h1 + h2, l1 + l2, loc='best')
+    fig.savefig('rnn_norms.jpg')
+    plt.clf()
+
+    X, y, x_series = X[:10], y[:10], x_series[:10]
+    # X, y, x_series = create_batch_func_params(1500, 0.1, 10)
 
     prediction = net.fprop(X)
     # plt.close('all')
-    f, axarr = plt.subplots(len(X), sharex=True, figsize=(160, 100))
+    fig, axarr = plt.subplots(len(X), sharex=True, figsize=(160, 5 * X.shape[0]))
     for i in range(len(X)):
         # axarr[i].figure.figure = plt.figure(figsize=(150, 12), dpi=100)
-        axarr[i].plot(prediction[i], label='model')
-        axarr[i].plot(y[i], label='actual')
-        axarr[i].plot(np.abs(y[i] - prediction[i]), label='diff')
-        axarr[i].legend()
+        axarr[i].set_title('freq:' + str(X[i][i]))
+        axarr[i].plot(prediction[i], label='model', color='blue')
+        axarr[i].plot(y[i], label='actual', color='green')
+        axarr_twin = axarr[i].twinx()
+        axarr_twin.plot(np.abs(y[i] - prediction[i]), label='error', color='red')
+        h1, l1 = axarr[i].get_legend_handles_labels()
+        h2, l2 = axarr_twin.get_legend_handles_labels()
+        axarr[i].legend(h1 + h2, l1 + l2, loc=0)
 
-    f.savefig('rnn_prediction_vs_actual.jpg')
+    fig.savefig('rnn_prediction_vs_actual.jpg')
 
     print "done"
 
--- utils.py
+++ utils.py
@@ -1,9 +1,21 @@
 __author__ = 'mike'
 
 import theano
-from theano import ifelse
+from theano.ifelse import ifelse
 from theano import tensor as T
 import numpy as np
+
+
+def float32(k):
+    return np.cast['float32'](k)
+
+
+def int32(k):
+    return np.cast['int32'](k)
+
+
+def relu(x):
+    return T.maximum(0, x)
 
 
 def variance(input_size):
@@ -15,8 +27,9 @@ def negative_log_likelihood(output, prediction):
     return -T.mean(T.log(output)[prediction])
 
 
-def adam(loss, all_params, const=[], learning_rate=0.001, b1=0.9, b2=0.999, e=1e-8,
-         gamma=1 - 1e-8, threshold=6):
+def sgd(loss, all_params, grads=None, const=[], learning_rate=0.001, b1=0.9, b2=0.999, e=1e-8,
+        gamma=1 - 1e-8, grad_threshold=10., momentum_threshold=1., ema_n=0.999, max_drawdown=0.4, drawdown_dumping=0.4,
+        training_step=None):
     """
     Code taken from https://gist.github.com/skaae/ae7225263ca8806868cb
 
@@ -31,10 +44,65 @@ def adam(loss, all_params, const=[], learning_rate=0.001, b1=0.9, b2=0.999, e=1e
 
     """
     updates = []
-    all_grads = theano.grad(loss, all_params, consider_constant=const)
-    norm_theta = T.sqrt(sum([(grad ** 2).sum() for grad in all_grads]))
-    all_grads = ifelse.ifelse(norm_theta > threshold, [(grad * threshold) / norm_theta for grad in all_grads],
-                              all_grads)
+    grad_threshold = theano.shared(float32(grad_threshold))
+    if grads:
+        all_grads = grads
+    else:
+        all_grads = theano.grad(loss, all_params, consider_constant=const)
+
+    grad_norm = T.sqrt(sum([(grad ** 2).sum() if grad.name != 'init_activationq' else 0 for grad in all_grads]))
+    all_grads = ifelse(grad_norm > grad_threshold,
+                       [(grad * grad_threshold) / grad_norm if grad.name != 'init_activationq' else grad for grad in
+                        all_grads],
+                       all_grads)
+
+    updates.append((grad_threshold, ema_n * grad_threshold + (1 - ema_n) * grad_norm))
+
+    for theta_previous, grad in zip(all_params, all_grads):
+        updates.append((theta_previous, theta_previous - learning_rate * grad))
+
+    return updates, grad_norm, grad_threshold, theano.shared(0), theano.shared(0)
+
+
+def adam(loss, all_params, grads=None, const=[], learning_rate=0.001, b1=0.9, b2=0.999, e=1e-8,
+         gamma=1 - 1e-8, grad_threshold=20., momentum_threshold=.5, ema_n=0.991, max_drawdown=1.0,
+         drawdown_dumping=.99, training_step=None):
+    """
+    Code taken from https://gist.github.com/skaae/ae7225263ca8806868cb
+
+    ADAM update rules
+    Default values are taken from [Kingma2014]
+
+    References:
+    [Kingma2014] Kingma, Diederik, and Jimmy Ba.
+    "Adam: A Method for Stochastic Optimization."
+    arXiv preprint arXiv:1412.6980 (2014).
+    http://arxiv.org/pdf/1412.6980v4.pdf
+
+    """
+    updates = []
+    param_updates = []
+    grad_threshold = theano.shared(float32(grad_threshold))
+    momentum_threshold = theano.shared(float32(momentum_threshold))
+    if grads:
+        all_grads = grads
+    else:
+        all_grads = theano.grad(loss, all_params, consider_constant=const)
+    grad_norm = T.sqrt(sum([(grad ** 2).sum() if grad.name != 'init_activationq' else 0 for grad in all_grads]))
+    all_grads = ifelse(grad_norm > grad_threshold,
+                       [(grad * grad_threshold) / grad_norm if grad.name != 'init_activationq' else grad for grad in
+                        all_grads],
+                       all_grads)
+
+    updates.append((grad_threshold, ema_n * grad_threshold + (1 - ema_n) * grad_norm))
+
+    # for theta_previous, grad in zip(all_params, all_grads):
+    # updates.append((theta_previous, theta_previous - learning_rate * grad))
+    #
+    # return updates, grad_norm
+
+
+    l_previous = theano.shared(np.float32(0))
     alpha = learning_rate
     t = theano.shared(np.float32(1))
     b1_t = b1 * gamma ** (t - 1)  # (Decay the first moment running average coefficient)
@@ -44,21 +112,43 @@ def adam(loss, all_params, const=[], learning_rate=0.001, b1=0.9, b2=0.999, e=1e
                                             dtype=theano.config.floatX))
         v_previous = theano.shared(np.zeros(theta_previous.get_value().shape,
                                             dtype=theano.config.floatX))
+        m, v = m_previous, v_previous
 
-        m = b1_t * m_previous + (1 - b1_t) * g  # (Update biased first moment estimate)
-        v = b2 * v_previous + (1 - b2) * g ** 2  # (Update biased second raw moment estimate)
+        # m, v = ifelse((l_previous - loss) / l_previous < max_drawdown, [m, v],
+        # [m * np.float32(drawdown_dumping), v * np.float32(drawdown_dumping)])
+
+        # m, v = ifelse(T.eq(T.mod(training_step, 10), 0), [m, v],
+        #               [m * np.float32(drawdown_dumping), v * np.float32(drawdown_dumping)])
+
+        m = b1_t * m + (1 - b1_t) * g  # (Update biased first moment estimate)
+        v = b2 * v + (1 - b2) * g ** 2  # (Update biased second raw moment estimate)
         m_hat = m / (1 - b1 ** t)  # (Compute bias-corrected first moment estimate)
         v_hat = v / (1 - b2 ** t)  # (Compute bias-corrected second raw moment estimate)
-        theta = theta_previous - (alpha * m_hat) / (T.sqrt(v_hat) + e)  # (Update parameters)
+        theta_update = (alpha * m_hat) / (T.sqrt(v_hat) + e)  # (Update parameters)
 
         updates.append((m_previous, m))
         updates.append((v_previous, v))
-        updates.append((theta_previous, theta))
+        param_updates.append(theta_update)
     updates.append((t, t + 1.))
-    return updates
+    updates.append((l_previous, loss))
+
+    momentum_norm = T.sqrt(
+        sum([(param ** 2).sum() if param.name != 'init_activation' else 0 for param in param_updates]))
+    param_updates = ifelse(momentum_norm > momentum_threshold,
+                           [(grad * momentum_threshold) / momentum_norm if grad.name != 'init_activation' else grad for
+                            grad
+                            in
+                            param_updates],
+                           param_updates)
+    updates.append((momentum_threshold, ema_n * momentum_threshold + (1 - ema_n) * momentum_norm))
+
+    for theta_previous, g in zip(all_params, param_updates):
+        updates.append((theta_previous, theta_previous - g))
+
+    return updates, grad_norm, grad_threshold, momentum_norm, momentum_threshold
 
 
 def quadratic_loss(a, b):
-    return T.mean(T.mean((b - a) ** 2, axis=0), axis=0).sum()
+    return T.mean((b - a) ** 2, axis=0).sum()
 
 
