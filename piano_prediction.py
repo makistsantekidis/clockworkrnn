722bebd3ff21f24110f6a698cec3bcd26b55b344
Avraam Tsantekidis <makistsantekidis@gmail.com>
2015-04-06 16:14:04 +0300

    added batch size for piano implementation

--- piano_prediction.py
+++ piano_prediction.py
@@ -12,9 +12,10 @@ floatX = theano.config.floatX
 
 def hot_to_sparse(data, size=88):
     result = []
+    length = 254
     for a in data:
-        song = np.zeros((1788, size), dtype=floatX)
-        for i, b in enumerate(a):
+        song = np.zeros((length, size), dtype=floatX)
+        for i, b in enumerate(a[:length]):
             # offset
             b = np.array(b, dtype='int32') - 21
             song[i, b] = 1
@@ -24,8 +25,11 @@ def hot_to_sparse(data, size=88):
 
 def main():
     nottingham = pickle.load(file("Nottingham.pickle"))
-    train = hot_to_sparse(nottingham['train'][:20], 88)
+    train = hot_to_sparse(nottingham['train'], 88)
+    print len(train)
+    np.random.shuffle(train)
     net = ClockworkRNN((88, (4, 30), 88), update_fn=adam, learning_rate=0.001, cost=quadratic_loss)
+    batch_size = 700
     losses = []
     lrs = []
     norms, momentum_norms = [], []
@@ -36,12 +40,20 @@ def main():
     decrement = float32(0.99)
     for i in range(2000):
         start = time()
-        loss, norm, norm_theshold, momentum_norm, momentum_norm_threshold = net.bptt(train[:, :-1], train[:, 1:])
-        losses.append(loss)
-        norms.append(norm)
-        momentum_norms.append(momentum_norm)
-        norms_thres.append(float32(norm_theshold))
-        momentum_norms_thres.append(float32(momentum_norm_threshold))
+        closs, cnorm, cnorm_threshold, cmomentum_norm, cmomentum_norm_threshold = [], [], [], [], []
+        for g in xrange(0, len(train), batch_size):
+            loss, norm, norm_theshold, momentum_norm, momentum_norm_threshold = net.bptt(train[g:g+batch_size, :-1], train[g:g+batch_size, 1:])
+            closs.append(loss)
+            cnorm.append(norm)
+            cnorm_threshold.append(norms_thres)
+            cmomentum_norm.append(momentum_norm)
+            cmomentum_norm_threshold.append(momentum_norm_threshold)
+
+        losses.append(np.mean(closs))
+        norms.append(np.mean(cnorm))
+        momentum_norms.append(np.mean(cnorm_threshold))
+        norms_thres.append(np.mean(float32(cnorm_threshold)))
+        momentum_norms_thres.append(np.mean([float32(cnmt) for cnmt in cmomentum_norm_threshold]))
         lrs.append(net.learning_rate.get_value())
         epoch_time = time() - start
         print i, ':', losses[-1], " took :", epoch_time
